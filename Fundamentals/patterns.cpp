122677c00944382fb00da271e54d143f708678e6
rajneeshgithub73 <rajneeshrj73@gmail.com>
2023-08-31 21:45:17 +0530

    update

--- Fundamentals/patterns.cpp
+++ Fundamentals/patterns.cpp
@@ -81,6 +81,37 @@ int main()
         }
         cout << endl;
     }
-    
+
+    // pattern 4
+
+    // *                   *
+    //   *               *
+    //     *           *
+    //       *       *
+    //         *   *
+    //           *
+
+    row = 6;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j < row - i; j++)
+        {
+            if (j == 0 || j == row - i - 1)
+            {
+                cout << "*   ";
+            }
+            else
+            {
+                cout << "    ";
+            }
+        }
+        cout << endl;
+    }
+
     return 0;
 }
