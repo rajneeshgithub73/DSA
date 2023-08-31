53bbc78e0afea80c0539afed0e3aba8e4b6f6ec6
rajneeshgithub73 <rajneeshrj73@gmail.com>
2023-08-31 21:25:48 +0530

    update

--- Fundamentals/patterns.cpp
+++ Fundamentals/patterns.cpp
@@ -1,7 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
     // pattern 1
 
@@ -11,16 +12,45 @@ int main(){
     //   *   *   *   *
     // *   *   *   *   *
 
-    int row = 10;
+    int row = 5;
 
-    for(int i=0; i<row; i++){
-        for(int j=0; j<row-i-1; j++){
-            cout<<"  ";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row - i - 1; j++)
+        {
+            cout << "  ";
         }
-        for(int j=0; j<i+1; j++){
-            cout<<"*   ";
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*   ";
         }
-        cout<<endl;
+        cout << endl;
     }
+
+    // pattern 2
+
+    //   *   *   *   *   *   *   *
+    //     *   *   *   *   *   *
+    //       *   *   *   *   *
+    //         *   *   *   *
+    //           *   *   *
+    //             *   *
+    //               *
+
+    row = 5;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j < row - i; j++)
+        {
+            cout << "*   ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
