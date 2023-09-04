9b00f63291b25a17f0fdb0b21b641db1cc2c87b9
rajneeshgithub73 <rajneeshrj73@gmail.com>
2023-09-04 21:00:09 +0530

    update

--- Fundamentals/patterns.cpp
+++ Fundamentals/patterns.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+    int row;
+    int col;
 
     // pattern 1
 
@@ -12,7 +14,7 @@ int main()
     //   *   *   *   *
     // *   *   *   *   *
 
-    int row = 5;
+    row = 5;
 
     for (int i = 0; i < row; i++)
     {
@@ -139,5 +141,89 @@ int main()
         cout << endl;
     }
 
+    cout << endl;
+
+    // *                 *
+    // * *             * *
+    // * * *         * * *
+    // * * * *     * * * *
+    // * * * * * * * * * *
+    // * * * * * * * * * *
+    // * * * *     * * * *
+    // * * *         * * *
+    // * *             * *
+    // *                 *
+
+    row = 10;
+
+    for (int r = 0; r < row; r++)
+    {
+        if (r < 5)
+        {
+            for (int c = 0; c <= r; c++)
+            {
+                cout << "* ";
+            }
+            for (int c = 0; c < row - 2 * r - 2; c++)
+            {
+                cout << "  ";
+            }
+            for (int c = 0; c <= r; c++)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            for (int c = 0; c < row - r; c++)
+            {
+                cout << "* ";
+            }
+            for (int c = 0; c < 2 * r - row; c++)
+            {
+                cout << "  ";
+            }
+            for (int c = 0; c < row - r; c++)
+            {
+                cout << "* ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    cout << endl;
+
+    // Hollow Rectangle
+    // * * * * * * * * * *
+    // *                 *
+    // *                 *
+    // *                 *
+    // * * * * * * * * * *
+
+    row = 5;
+    col = 10;
+
+    for (int r = 0; r < row; r++)
+    {
+        for (int c = 0; c < col; c++)
+        {
+            if (r == 0 || r == row - 1)
+            {
+                cout << "* ";
+            }
+            else if (c == 0 || c == col - 1)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    
     return 0;
 }
