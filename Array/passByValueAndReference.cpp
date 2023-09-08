38e5c3d47a0d8e1449661c9cb018b80826844c24
rajneeshgithub73 <rajneeshrj73@gmail.com>
2023-09-08 21:38:09 +0530

    update

---
+++ Array/passByValueAndReference.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void passByValue(int a)
+{
+    a++;
+}
+
+void passByRefrence(int &b)
+{
+    b++;
+}
+
+int main()
+{
+    int a = 5;
+
+    passByValue(a);
+
+    int b = 10;
+
+    passByRefrence(b);
+
+    cout << "a : " << a << endl;
+    cout << "b : " << b << endl;
+
+    return 0;
+}
